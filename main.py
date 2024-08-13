9ffd9babf0c3f51283ed6b73ef08f8f32e11d3d9
Hayato Shibahara <hayatoshibahara@gmail.com>
2024-08-13 15:46:51 +0900

    chore: Add images API example

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def index(request: Request):
 
 
 @app.post("/completions", response_class=HTMLResponse)
-async def generate(request: Request, prompt: str = Form(...)):
+async def completions(request: Request, prompt: str = Form(...)):
     """
     Chat Completions API を使用し、ユーザーの入力に対して回答を生成します。
     https://platform.openai.com/docs/guides/chat-completions
@@ -48,6 +48,41 @@ async def generate(request: Request, prompt: str = Form(...)):
 
     return templates.TemplateResponse(
         "index.html", {"request": request, "message": message}
+    )
+
+
+@app.post("/images", response_class=HTMLResponse)
+async def dalle(request: Request, prompt: str = Form(...)):
+    """
+    Images API を使用し、ユーザーの入力に対して画像を生成します。
+    https://platform.openai.com/docs/guides/images/usage?context=python
+    """
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": f"""
+                以下のユーザの入力に対して「細部まで緻密に描かれた高品質なアイコン画像」を
+                出力するDALL-Eのプロンプトをできる限り詳細を含めて作成してください：
+                {prompt}
+                """,
+            },
+        ],
+    )
+
+    image_prompt = response.choices[0].message.content
+
+    response = client.images.generate(
+        model="dall-e-3", prompt=image_prompt, size="1024x1024", quality="standard", n=1
+    )
+
+    image_url = response.data[0].url
+
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "image_prompt": image_prompt, "image_url": image_url},
     )
 
 
